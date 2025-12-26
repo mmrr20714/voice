bc353189556e7908c401b9698d45c63743e5b990
mmrr20714 <mmrr20714@gmail.com>
2025-12-26 10:45:37 +0330

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ io.on('connection', (socket) => {
   console.log('یوزی جدید اومد پارتی:', socket.id);
   socket.username = "ناشناس شیطون";
 
+  // به خودش لیست کاربرهای فعلی رو بده (بدون خودش)
+  socket.emit('current-users', Array.from(io.sockets.sockets.keys()).filter(id => id !== socket.id));
+
+  // به همه بگو یکی جدید اومد (با آیدی)
+  socket.broadcast.emit('user-joined', socket.id);
+
+  // به خودش هم بگو که سوند جوین پخش کنه
+  socket.emit('user-joined', socket.id);
+
   socket.on('set-name', (name) => {
     socket.username = name.trim() || "Brozone جون";
     io.emit('user-list', getUserList());
@@ -17,9 +26,6 @@ io.on('connection', (socket) => {
   socket.on('chat', (msg) => {
     io.emit('chat', { name: socket.username, msg });
   });
-
-  socket.broadcast.emit('user-joined');
-  socket.emit('user-list', getUserList());
 
   socket.on('signal', (data) => {
     socket.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
